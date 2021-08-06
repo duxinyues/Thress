66c39b93b444353045ebb0110f5f4229981d1a5f
duxinyues <yongyuan253015@gmail.com>
2021-08-06 21:52:11 +0800

    读取文件模快

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: yongyuan at <yongyuan253015@gmail.com>
  * @Date: 2021-08-05 22:37:45
- * @LastEditTime: 2021-08-05 22:38:36
+ * @LastEditTime: 2021-08-06 21:38:24
  * @LastEditors: yongyuan at <yongyuan253015@gmail.com>
  * @Description: 
  * @FilePath: \nodeJS\index.js
@@ -15,4 +15,13 @@ const server = http.createServer((request, response) => {
 });
 server.listen(8080);
 
-console.log("dvdfb")
+const fs = require('fs');
+fs.readFile("./test.txt", { "encoding": "utf8" }, function (err, data) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log(data)
+})
+
+console.log("read async")
